fd7cb56a460277c9052d02b8c31c547cb85c605b
M-J-Christina213 <m.j.christina213@gmail.com>
2025-12-23 18:44:38 +0530

    added a profile pic 😉

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import profilePic from "../../assets/images/christina.jpg"; // replace with your image
+import profilePic from "../../assets/images/profile.jpeg"; 
 import { FaJs, FaReact, FaFlutter } from "react-icons/fa";
 
 const roles = [
